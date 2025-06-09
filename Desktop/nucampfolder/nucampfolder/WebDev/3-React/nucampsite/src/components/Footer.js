c57020218ecdbf087e6b474538cc3f87b9623be4
zouzou <dallagizouhour0@gmail.com>
2025-06-09 23:18:21 +0100

    Header and Footer Links

--- Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/components/Footer.js
+++ Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-
+import {Link} from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className="site-footer">
@@ -8,6 +8,20 @@ const Footer = () => {
         <Row>
           <Col xs={{ size: 4, offset: 1 }} sm="2">
             <h5>this will be site navigation!</h5>
+             <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
           </Col>
 
           <Col xs="6" sm="3" className="text-center">
--- Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/components/Header.js
+++ Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/components/Header.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { useState} from 'react';
+
 import NucampLogo from '../app/assets/img/logo.png';
 
+import {Navbar,NavbarBrand, Collapse,NavbarToggler,Nav,NavItem} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
+
 const Header = () => {
+  const [menuOpen,setMenuOpen]=useState(false);
   return (
-    <Navbar dark color="primary">
-              <NavbarBrand href="/">
-                 <img src={NucampLogo} className="App-logo" alt="logo" />
+    <Navbar dark color="primary" sticky='top' expand='md'>
+              <NavbarBrand className='ms-5'href="/">
+                 <img src={NucampLogo} className="nucamp logo" alt="nucamp logo" />
+                 <h1 className='mt-1'>nucamp</h1>
               </NavbarBrand>
+              <NavbarToggler onClick={()=>setMenuOpen(!menuOpen)}/>
+              <Collapse isOpen={menuOpen} navbar>
+              <Nav className='ms-auto' navbar>
+                <NavItem>
+                    <NavLink className='nav-link' to='/'>
+                        <i className='fa fa-home fa-lg' /> Home
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='nav-link' to='/directory'>
+                        <i className='fa fa-list fa-lg' /> Directory
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='nav-link' to='/about'>
+                        <i className='fa fa-info fa-lg' /> About
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='nav-link' to='/contact'>
+                        <i className='fa fa-address-card fa-lg' /> Contact
+                    </NavLink>
+                </NavItem>
+             </Nav>
+             </Collapse>
             </Navbar>
   
   );
