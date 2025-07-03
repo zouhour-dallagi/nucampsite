e596e73facdb7638fe29a907725e697d763c195c
zouzou <dallagizouhour0@gmail.com>
2025-07-03 20:23:41 +0100

    Error and Loading

--- Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/features/campsites/CampsitesList.js
+++ Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/features/campsites/CampsitesList.js
@@ -1,13 +1,32 @@
 import React from "react";
 import{Row,Col} from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+//import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import { useSelector } from "react-redux";
 import { selectAllCampsites } from "./campsitesSlice";
+import {Error,Loading} from "../../components/Error";
 //import { selectAllCampsites } from "./campsitesSlice";
 const CampsitesList=({setCampsiteId})=>{
 const campsites=useSelector(selectAllCampsites);
+ const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
 console.log('campsites',campsites)
+  
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    }
 
   return (
     <Row className="ms-auto">
--- Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/pages/CampsiteDetail.js
+++ Desktop/nucampfolder/nucampfolder/WebDev/3-React/nucampsite/src/pages/CampsiteDetail.js
@@ -3,28 +3,25 @@ import CommentsList from "../features/comments/CommentsList";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectAllCampsiteById } from "../features/campsites/campsitesSlice";
-const CampsiteDetail=()=>{
-    const {campsiteId}= useParams();
-     const campsite = useSelector(selectAllCampsiteById(campsiteId));
-    console.log('campsite:', campsite);
-    if (!campsite) {
-        return <p>Campsite not found</p>;
-    }
-      const { image, name, description, comments, id } = campsite;
-    return(
-         <>
-       <Col md='12' className='m-4'>
-            <Card>
-                <CardImg top src={image} alt={name}/>
-                <CardBody>
-                    <CardText>{description}</CardText>
-                </CardBody>
-            </Card>    
-        </Col>
-        <Col md="12" className="m-4">
-        <CommentsList comments={comments} campsiteId={id} />
-      </Col>
-    
-</>);
-}
+const CampsiteDetail = () => {
+    const { campsiteId } = useParams();
+    const campsite = useSelector(selectCampsiteById(campsiteId));
+    console.log('campsite', campsite);
+
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+    let content = null;
+
+    if (isLoading) {
+        content = <Loading />;
+    } else if (errMsg) {
+        content = <Error errMsg={errMsg} />;
+    } else {
+        content = (
+            <>
+                <CampsiteDetail campsite={campsite} />
+                <CommentsList campsiteId={campsiteId} />
+            </>
+        );
+    }}
 export default CampsiteDetail;
